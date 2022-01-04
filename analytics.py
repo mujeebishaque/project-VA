b38790f0c489d0a8678a9d38ab4a9936810a97db
Mujeeb Ishaque <mujeeb.ishaque@gmail.com>
2022-01-04 10:03:38 +0100

    email for Prof. santucci

---
+++ analytics.py
@@ -0,0 +1,18 @@
+import sys, os
+
+'''
+Prof. Giuseppe Santucci
+Email: santucci@diag.uniroma1.it
+https://www.kaggle.com/yamqwe/ebay-uk-products
+The dataset contains scraped products from eBay. This dataset was created by CrawlFeeds and contains around 3000 product names and prices along with other features such as:
+Is it in stock? price, brand.
+What currency was it listed with
+And more.
+'''
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
